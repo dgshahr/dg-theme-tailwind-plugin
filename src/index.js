f271b56515987510befa38a2c2fec0f0ad2d6f25
nazari2079 <mohammadnazary2000@gmail.com>
2024-08-22 17:18:13 +0330

    fix: remove fonts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const plugin = require("tailwindcss/plugin");
-const danaFont = require("../assets/fonts");
 const colors = require("./constants/colors");
 const shadows = require("./constants/shadows");
 const utilities = require("./utilities");
 
 const DgThemeTailwindPlugin = plugin(
-  function ({ addBase, addUtilities }) {
-    addBase(danaFont);
+  function ({ addUtilities }) {
     addUtilities(utilities);
   },
   {
